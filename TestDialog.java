58f4472877b04d265fd60a9a5ffe4c73b7fe5fc8
elleinaretri <42725738+elleinaretri@users.noreply.github.com>
2018-09-13 21:15:35 +0700

    Add files via upload

---
+++ TestDialog.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DaftarAntrianDialog;
+
+/**
+ *
+ * @author ASUS
+ */
+public class TestDialog {
+    public static void main(String[] args) {
+        DaftarAntrianDialog test = new DaftarAntrianDialog("Test");
+        test.setSize(300,400);
+        test.setVisible(true);
+    }
+}
